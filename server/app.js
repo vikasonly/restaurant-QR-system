8f991294134fd73562a2b357adb0b2b4e77ec1a0
vikaskirdoliya <134048517+vikaskirdoliya@users.noreply.github.com>
2025-11-29 12:25:03 +0530

    fix api route

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const app=express();
 
+app.get('/',(req,res)=>{
+    req.send("Homepage")
+})
 app.listen(3000,()=>{
     console.log(`server is running on port 3000`)
 })
